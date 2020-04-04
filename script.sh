6de0c9565556a3cb961b535acaa0115524cc029f
vbhave <vbhave@umass.edu>
2020-04-04 16:36:06 -0400

    ready to ship to dan

--- script.sh
+++ script.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
+mkdir output
 directory_name=/home/osboxes/Desktop/scene_detection/directory_images/
-#file_count=$(ls /home/osboxes/Desktop/scene_detection/directory_images | wc -l)
-#echo "directory name is    "
-#echo "$directory_name"
 file_count=$(ls $directory_name | wc -l)
-#echo $file_count
 
 progress-bar() {
   local duration=${1}
@@ -22,10 +19,8 @@ progress-bar() {
 python3 db_setup.py
 
 counter=1
-#for entry in "/home/osboxes/Desktop/scene_detection/directory_images"/*
 for entry in $(ls $directory_name)
-do 
-  #echo "$entry"
+do  
   python3 unified.py $directory_name/$entry
   progress-bar counter
   counter=$((counter + 1))
